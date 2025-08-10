70a82f2a3475d765cfb2f7defb5f11f5295768a2
Anastasiia <kalitina2307@gmail.com>
2025-08-10 20:40:57 +0300

    worker with DateTime and LocalDate classes

--- workers/Worker.java
+++ workers/Worker.java
@@ -1,20 +1,23 @@
 package workers;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Worker implements Comparable<Worker> {
     private String name;
     private String position;
-    private int yearOfAppointment;
+    private String dataOfAppointment;
 
     public Worker() {
     }
 
-    public Worker(String name, String position, int yearOfAppointment) {
+    public Worker(String name, String position, String dataOfAppointment) {
         this.name = name;
         this.position = position;
-        this.yearOfAppointment = yearOfAppointment;
+        this.dataOfAppointment = dataOfAppointment;
     }
 
     public String getName() {
@@ -25,17 +28,18 @@ public class Worker implements Comparable<Worker> {
         return position;
     }
 
-    public int getYearOfAppointment() {
-        return yearOfAppointment;
+    public String getDataOfAppointment() {
+        return dataOfAppointment;
     }
 
     public String toString() {
         return "Ім'я та прізвище: " + getName() +
                 ", посада: " + getPosition() +
-                ", рік прийняття: " + getYearOfAppointment();
+                ", дата прийняття на посаду: " + getDataOfAppointment();
     }
 
     Worker[] workers = new Worker[5];
+
     public void displayInfo() {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < workers.length; i++) {
@@ -43,21 +47,26 @@ public class Worker implements Comparable<Worker> {
             String name = scanner.nextLine();
             System.out.println("Введіть посаду працівника: ");
             String position = scanner.nextLine();
-            int yearOfAppointment;
+            String dataOfAppointment;
             while (true) {
+                System.out.println("Введіть дату прийняття працівника на посаду у форматі dd.MM.yyyy з урахуванням, що компанія працює з 2000 року: ");
+                dataOfAppointment = scanner.nextLine();
+                DateTimeFormatter date = DateTimeFormatter.ofPattern("dd.MM.yyyy");
                 try {
-                    System.out.println("Введіть рік прийняття працівника на посаду з урахуванням, що компанія працює з 2000 року: ");
-                    yearOfAppointment = scanner.nextInt();
-                    scanner.nextLine();
-                    if (yearOfAppointment < 2000) {
-                        throw new IllegalAccessException("Невірно введений рік прийняття на роботу");
-                    }break;
-                } catch (Exception e) {
-                    System.out.println("Введіть рік прийняття працівника на посаду у вигляді числа");
-                }
-            }
+                    LocalDate localDate = LocalDate.parse(dataOfAppointment, date);
+                    LocalDate minDate = LocalDate.of(2000, 1, 1);
+                    if (localDate.isBefore(minDate)) {
+                        System.out.println("Дата не може бути раніше 01.01.2000");
+                    }
 
-            workers[i] = new Worker(name, position, yearOfAppointment);
+                } catch (DateTimeParseException e) {
+                    System.out.println("Невірний формат або неіснуюча дата.");
+                }
+
+                break;
+            }
+            workers[i] = new Worker(name, position, dataOfAppointment);
+
         }
 
         Arrays.sort(workers);
@@ -75,14 +84,23 @@ public class Worker implements Comparable<Worker> {
         }
         return result;
     }
-    public void workExperience(){
-        for(Worker temp : workers){
-            if(temp.getYearOfAppointment() < 2010){
-                System.out.println(temp);
+
+    public void workExperience() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate minDate = LocalDate.of(2010, 1, 1);
+        for (Worker temp : workers) {
+            try {
+                LocalDate hireDate = LocalDate.parse(temp.dataOfAppointment, formatter);
+                if (hireDate.isBefore(minDate)) {
+                    System.out.println(temp);
+                }
+            } catch (DateTimeParseException e) {
+                System.out.println("Невірний формат дати у працівника: " + temp.name);
             }
         }
     }
 }
+
 
 class Main {
     public static void main(String[] args) {
